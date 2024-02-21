69df1b4d3aad771f1ae7241343a3c39091e44127
Anujeet Swain <anujeet98@gmail.com>
2024-02-21 07:02:08 +0530

    Aws serverless configuration added along with mongodb connection.

--- entries.js
+++ entries.js
@@ -1,19 +1,10 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const adapter = new FileSync('profile-database.json')
-const db = low(adapter)
-db.defaults({ entries: [] }).write()
-
-function putEntry(entry) {
-    db.get('entries').push(entry).write();
-}
-
-function getEntries() {
-    return db.get('entries').value()
-}
-
-module.exports = {
-    putEntry,
-    getEntries,
-}
+const EntriesSchema = new Schema({
+    entry: {
+        type: Object,
+        required: true
+    },
+});
+module.exports = mongoose.model('MsgDb', EntriesSchema);
--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-let express = require('express')
-var morgan = require('morgan')
-let cors = require('cors')
-let bodyParser = require('body-parser')
-let entries = require('./entries')
-const dotenv = require('dotenv')
+require('dotenv').config();
+const serverless = require("serverless-http");
+const express = require("express");
+let cors = require('cors');
+const mongoose = require('mongoose');
+let Entries = require('./entries');
 
-dotenv.config()
-let app = express()
-app.use(morgan('dev'))
+
+const app = express();
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json());
 
-app.get('/', (_, res) => {
-    res.send('Hello from the profile backend!')
-})
-
-app.get('/entries', (_, res) => {
-    res.json(entries.getEntries())
-})
-
-app.post('/entries', (req, res) => {
-    console.log('Data received - ', req.body)
-    entries.putEntry(req.body)
-    console.log('Stored successfully!')
-    res.sendStatus(200)
-})
-
-app.listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Profile backend running on ${process.env.PORT} ...`)
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from the profile backend!",
+  });
 });
+
+app.get("/entries", async(req, res, next) => {
+  const result = await Entries.find();
+  return res.status(200).json({
+    message: result,
+  });
+});
+
+
+app.post('/entries', async(req, res, next) => {
+  const newEntry = new Entries({entry:req.body});
+  await newEntry.save();
+  res.sendStatus(200);
+})
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+const con = async() => {
+  await mongoose.connect(`${process.env.MONGODB_CONN_STR}`);
+};
+con();
+module.exports.handler = serverless(app);
